c78812d08c88493c66de25235634d3967453eace
Robby Dhindsa <dhindsa.1996@gmail.com>
2023-03-02 13:11:27 -0800

    Add Panel component and import into Dashboard

---
+++ src/components/Panel.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export default class Panel extends Component {
+  render() {
+    const { id, label, value, onSelect } = this.props;
+
+    return (
+      <section
+        className="dashboard__panel"
+        onClick={event => onSelect(id)}
+      >
+        <h1 className="dashboard__panel-header">{label}</h1>
+        <p className="dashboard__panel-value">{value}</p>
+      </section>
+    );
+  }
+}
